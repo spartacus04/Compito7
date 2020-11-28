2ad278b5563e7962a8cf4363d56a593bb6ae27c2
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2020-11-28 10:01:09 +0100

    Rifinimento Esercizio

--- Program.cs
+++ Program.cs
@@ -1,53 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Compito7
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.Write("Inserire un mese: ");
-            int mese = int.Parse(Console.ReadLine());
-            Console.Write("Inserire un giorno: ");
-            int giorno = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.Clear();
+                Console.Write("Inserire un giorno: ");
+                int giorno = int.Parse(Console.ReadLine());
+                Console.Write("Inserire un mese: ");
+                int mese = int.Parse(Console.ReadLine());
 
-            int num = (mese - 1) * 30 + giorno;
-            int resto = num % 7;
+                //DateTime date = new DateTime(DateTime.Now.Year, mese, giorno);
+                //Console.WriteLine($"Il giorno della settimana è {date.DayOfWeek} ");
 
-            if(resto == 1)
-            {
-                Console.WriteLine("Il giorno è lunedì");
-            }
-            else if(resto == 2)
-            {
-                Console.WriteLine("Il giorno è martedì");
-            }
-            else if(resto == 3)
-            {
-                Console.WriteLine("Il giorno è mercoledì");
-            }
-            else if (resto == 4)
-            {
-                Console.WriteLine("Il giorno è giovedì");
-            }
-            else if (resto == 5)
-            {
-                Console.WriteLine("Il giorno è venerdì");
-            }
-            else if (resto == 6)
-            {
-                Console.WriteLine("Il giorno è sabato");
-            }
-            else if (resto == 7)
-            {
-                Console.WriteLine("Il giorno è domenica");
-            }
+                int num = (mese - 1) * 30 + giorno;
+                int resto = num % 7;
 
-            Console.ReadKey();
+                Console.Write("Il giorno è ");
+                switch (resto)
+                {
+                    case 1:
+                        Console.WriteLine("lunedì");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("martedì");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("mercoledì");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("giovedì");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("venerdì");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("sabato");
+                        break;
+
+                    case 7:
+                        Console.WriteLine("domenica");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Errore:\n\n" + e);
+            }
+            finally
+            {
+                Console.WriteLine("\nPremere un tasto per continuare");
+                Console.ReadKey();
+            }
         }
     }
 }
